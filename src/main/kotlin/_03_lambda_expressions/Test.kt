2ec0d4e15408cc1dd7d2e870bac0e0e82ba56afc
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-01-26 21:51:18 +0000

    printSum: lambda expression with block body

--- src/main/kotlin/_03_lambda_expressions/Test.kt
+++ src/main/kotlin/_03_lambda_expressions/Test.kt
@@ -3,6 +3,10 @@ package _03_lambda_expressions
 val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
 val prod = { x: Int, y: Int -> x * y }
 val printProd = { x: Int, y: Int -> print(x * y) }
+val printSum = { x: Int, y: Int ->
+    val s = x + y
+    print(s)
+}
 
 fun main() {
     var a = 3
@@ -12,5 +16,9 @@ fun main() {
 
     print("$a * $b = ")
     printProd(a, b)
+    println()
+
+    print("$a + $b = ")
+    printSum(a, b)
     println()
 }
