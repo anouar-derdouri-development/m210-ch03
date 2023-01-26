b0e54468d065db213c7749ea8b397e49fe1b799d
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-01-26 21:43:02 +0000

    printProd: lambda expression that return Unit type

--- src/main/kotlin/_03_lambda_expressions/Test.kt
+++ src/main/kotlin/_03_lambda_expressions/Test.kt
@@ -2,10 +2,15 @@ package _03_lambda_expressions
 
 val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
 val prod = { x: Int, y: Int -> x * y }
+val printProd = { x: Int, y: Int -> print(x * y) }
 
 fun main() {
     var a = 3
     var b = 14
     println("$a + $b = ${sum(a, b)}")
     println("$a * $b = ${prod(a, b)}")
+
+    print("$a * $b = ")
+    printProd(a, b)
+    println()
 }
