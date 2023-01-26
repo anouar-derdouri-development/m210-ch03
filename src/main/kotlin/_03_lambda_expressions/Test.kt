387e5f989cbb4a627938984d52ee23cfcfb52b0e
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-01-26 21:38:01 +0000

    prod: lambda expression with inferred type

--- src/main/kotlin/_03_lambda_expressions/Test.kt
+++ src/main/kotlin/_03_lambda_expressions/Test.kt
@@ -1,9 +1,11 @@
 package _03_lambda_expressions
 
 val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
+val prod = { x: Int, y: Int -> x * y }
 
 fun main() {
     var a = 3
     var b = 14
     println("$a + $b = ${sum(a, b)}")
+    println("$a * $b = ${prod(a, b)}")
 }
